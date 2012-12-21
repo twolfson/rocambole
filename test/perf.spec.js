a541e3740a395b5d2db69f09d5e0dac8d1b0001f
millermedeiros <miller@millermedeiros.com>
2012-12-21 15:42:57 -0200

    moar perf. see #4
    made it even faster, but it's getting hard to squeeze more perf out of it:
    
    ```js
    esprima          : 1203ms
    instrumentTokens : 2328ms
    instrumentNodes  : 11178ms
    TOTAL            : 14709ms
    ```

--- test/perf.spec.js
+++ test/perf.spec.js
@@ -12,16 +12,20 @@ describe('performance', function () {
     it('should be fast :P', function () {
         var file = _fs.readFileSync( __dirname +'/files/crossroads.js', 'utf-8');
         var startTime = process.hrtime();
+    // console.time('TOTAL')
         var ast = walker.parse(file);
+    // console.timeEnd('TOTAL')
         var diff = process.hrtime(startTime);
         expect( diff[1] ).to.be.below( 400000000 );
         expect( ast.startToken ).not.to.be( undefined );
     });
 
-    xit('should not take forever to instrument jQuery', function () {
+    it('should not take forever to instrument jQuery', function () {
         var file = _fs.readFileSync( __dirname +'/files/jquery.js', 'utf-8');
         var startTime = process.hrtime();
+    // console.time('TOTAL')
         var ast = walker.parse(file);
+    // console.timeEnd('TOTAL')
         var diff = process.hrtime(startTime);
         expect( diff[0] ).to.be.below( 40000 );
         expect( ast.startToken ).not.to.be( undefined );
